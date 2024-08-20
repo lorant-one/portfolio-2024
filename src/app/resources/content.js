7f1180e56bdd7ca2d3ee010ded92e1c8968c4401
Lorant <lorant.toth.94@gmail.com>
2024-08-20 14:14:56 +0200

    edit content

---
+++ src/app/resources/content.js
@@ -0,0 +1,222 @@
+import { InlineCode } from "@/once-ui/components";
+
+const person = {
+    name:      'Lorant Toth',
+    role:      'Design Engineer',
+    avatar:    '/images/avatar.png',
+    location:  'Europe/Vienna',       // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+    languages: ['English', 'Hungarian']  // optional: Leave the array empty if you don't want to display languages
+}
+
+const newsletter = {
+    title: <>Subscribe to Alex's Newsletter</>,
+    description: <>I occasionally write about design, technology, and share thoughts on the intersection of creativity and engineering.</>
+}
+
+const social = [
+    // Links are automatically displayed.
+    // Import new icons in /once-ui/icons.ts
+    {
+        name: 'GitHub',
+        icon: 'github',
+        link: 'https://github.com/lorantart',
+    },
+    {
+        name: 'LinkedIn',
+        icon: 'linkedin',
+        link: 'https://www.linkedin.com/in/tothlorant',
+    },
+    {
+        name: 'X (Twitter)',
+        icon: 'x',
+        link: 'https://www.x.com/lorant_one',
+    },
+    {
+        name: 'Email',
+        icon: 'email',
+        link: 'mailto:lorant@once-ui.com',
+    },
+]
+
+const home = {
+    label: 'Home',
+    title: `${person.name}'s Portfolio`,
+    description: `Portfolio website showcasing my work as a ${person.role}`,
+    headline: <>Design engineer by day, creative by night</>,
+    subline: <>Helping designers code and developers design with Once UI</>
+}
+
+const about = {
+    label: 'About',
+    title: 'About me',
+    description: `Meet ${person.name}, ${person.role} from ${person.location}`,
+    tableOfContent: {
+        display: true,
+        subItems: true
+    },
+    avatar: {
+        display: true
+    },
+    calendar: {
+        display: true,
+        link: 'https://cal.com'
+    },
+    intro: {
+        display: true,
+        title: 'Introduction',
+        description: <>I’m a Vienna-based design engineer with a passion for solving complex problems through simple, elegant design solutions. My work spans digital interfaces, interactive experiences, and the convergence of design and technology.</>
+    },
+    work: {
+        display: true, // set to false to hide this section
+        title: 'Work Experience',
+        experiences: [
+            {
+                company: 'Once UI',
+                timeframe: '2022 - Present',
+                role: 'Senior Design Engineer',
+                achievements: [
+                    <>Redesigned the UI/UX for the Once UI platform, resulting in a 20% increase in user engagement and 30% faster load times.</>,
+                    <>Spearheaded the integration of AI tools into design workflows, enabling designers to iterate 50% faster.</>
+                ],
+                images: [ // optional: leave the array empty if you don't want to display images
+                    {
+                        src: '/images/projects/project-01/cover.jpg',
+                        alt: 'Once UI Project',
+                        width: 16,
+                        height: 9
+                    }
+                ]
+            },
+            {
+                company: 'Creative Labs',
+                timeframe: '2018 - 2022',
+                role: 'Lead Designer',
+                achievements: [
+                    <>Developed a design system that unified the brand across multiple platforms, improving design consistency by 40%.</>,
+                    <>Led a cross-functional team to launch a new product line, contributing to a 15% increase in overall company revenue.</>
+                ],
+                images: [ ]
+            }
+        ]
+    },
+    technical: {
+        display: true, // set to false to hide this section
+        title: 'Technical skills',
+        skills: [
+            {
+                title: 'Figma',
+                description: <>Include a concise overview of your key responsibilities, highlighting your direct impact on the organization, such as improved processes, cost savings, or revenue generation.</>,
+                images: [
+                    {
+                        src: '/images/projects/project-01/image-01.jpg',
+                        alt: 'Project image',
+                        width: 16,
+                        height: 9
+                    },
+                    {
+                        src: '/images/projects/project-01/image-03.jpg',
+                        alt: 'Project image',
+                        width: 16,
+                        height: 9
+                    },
+                ]
+            }
+        ]
+    }
+}
+
+const blog = {
+    label: 'Blog',
+    title: 'Writing about design and tech...',
+    description: `Read what ${person.name} has been up to recently`
+    // Create new blog posts by adding a new .mdx file to app/blog/posts
+    // All posts will be listed on the /blog route
+}
+
+const work = {
+    label: 'Work',
+    title: 'My projects',
+    description: `Design and dev projects by ${person.name}`
+    // Create new project pages by adding a new .mdx file to app/blog/posts
+    // All projects will be listed on the /home and /work routes
+}
+
+const gallery = {
+    label: 'Gallery',
+    title: 'My photo gallery',
+    description: `A photo collection by ${person.name}`,
+    images: [
+        { 
+            src: '/images/gallery/img-01.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-02.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-03.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-04.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-05.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-06.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-07.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-08.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-09.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-10.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-11.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-12.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-13.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-14.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+    ]
+}
+
+export { person, social, newsletter, home, about, blog, work, gallery };
