c6efb548e5f463fffe656b193502c56ebb6f377e
Lorant <lorant@once-ui.com>
2025-03-04 20:59:34 +0100

    message 5

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -18,7 +18,7 @@ export const Message = (
   return (
     <Row fillWidth horizontal="end" direction={type === "sender" ? "row" : "row-reverse"} vertical="end" gap="8">
       <Column className="fit-width" paddingX="20" paddingTop="8" paddingBottom="4" maxWidth={type === "sender" ? 24 : undefined} background={type === "sender" ? "brand-alpha-weak" : undefined} border={type === "sender" ? "brand-alpha-weak" : "neutral-alpha-medium"} radius="l" bottomRightRadius={type === "sender" ? "s" : undefined} bottomLeftRadius={type === "receiver" ? "s" : undefined} {...rest}>
-        <Text variant="body-default-m">{children}</Text>
+        <Text variant="body-default-m" wrap="balance">{children}</Text>
       </Column>
       <Avatar marginBottom="4" src={type === "sender" ? "/images/avatar.jpg" : "/images/blog/misc/quasar.jpg"} size="s"/>
     </Row>
